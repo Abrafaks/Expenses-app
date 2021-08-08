fda858b3a1d5953f98b89fdb642fd520e03787e9
Abrafaks <abrafakspl@gmail.com>
2021-08-08 21:12:06 +0200

    Add expense form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../../UI/Card.js";
 
 import "./ExpenseItem.css";
 
-function ExpenseItem({ title, date, amount } = {}) {
+function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const changeTitleHandler = () => {
+    setTitle("Groceries");
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date}></ExpenseDate>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={changeTitleHandler}>Change Title</button>
     </Card>
   );
 }
